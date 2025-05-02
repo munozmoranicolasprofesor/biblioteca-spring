37df1fa494048ba6aa4e32eb083eec0a1e882678
Nicolas Munoz <munoz.mora.nicolas@gmail.com>
2025-05-02 12:01:48 -0400

    fix

--- src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
+++ src/main/java/com/holamundo/ejemplo/holamundo/controller/HolaMundoController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
-
 @RestController
 public class HolaMundoController {
 
